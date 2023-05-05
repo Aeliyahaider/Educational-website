5ac9f77a6dbab58e6a90d7646c0bb1ed992cb76e
Aeliyahaider <132681388+Aeliyahaider@users.noreply.github.com>
2023-05-05 19:15:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+burgur = document.querySelector(',birgur')
+navbar = document.querySelector(',navbar')
+rightnav = document.querySelector(',rightnav')
+navlist = document.querySelector(',navlist')
+
+birgur.addEventlistiner ('click', ()=>{
+    rightnav.classlist.toggle('v-class-resp');
+    navlistist.classlist.toggle('v-class-resp');
+    navbar.classlist.toggle('h-nav-resp');
+})
+
+
+ 
+
